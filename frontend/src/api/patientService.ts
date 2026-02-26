2b5c5fb2b3ed3ca547f6bf6babc80cce2d5bc4e4
unknown <paulsong29@gmail.com>
2026-02-26 16:38:47 -0500

    added vite api url to frontend

--- frontend/src/api/patientService.ts
+++ frontend/src/api/patientService.ts
@@ -6,7 +6,7 @@ export interface Patient {
   check_in_time?: string;
 }
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const patientService = {
   // Sorted by priority
