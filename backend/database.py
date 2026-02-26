92309eac798b91834c5a43b4943d11f5bde8bef9
unknown <paulsong29@gmail.com>
2026-02-26 16:10:12 -0500

    added dockerfile for koyeb and changed db url for neon

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+connect_args = {}
+if "neon_tech" in SQLALCHEMY_DATABASE_URL:
+    connect_args = {"sslmode": "require"}
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args=connect_args
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
